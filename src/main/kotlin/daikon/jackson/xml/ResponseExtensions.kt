411851b2832307605333cfb0e53afddcfbea7da1
Alessio Coser <alessio.coser@xpeppers.com>
2020-04-10 12:35:48 +0200

    removes jetty http mime type

--- src/main/kotlin/daikon/jackson/xml/ResponseExtensions.kt
+++ src/main/kotlin/daikon/jackson/xml/ResponseExtensions.kt
@@ -2,10 +2,11 @@ package daikon.jackson.xml
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper
 import daikon.core.Response
-import org.eclipse.jetty.http.MimeTypes.Type.TEXT_XML_UTF_8
+
+const val TEXT_XML_UTF_8 = "text/xml;charset=utf-8"
 
 fun Response.xml(value: Any) {
-    this.type(TEXT_XML_UTF_8.asString())
+    this.type(TEXT_XML_UTF_8)
     val xml = XmlMapper.builder().build().writeValueAsString(value)
     this.write(xml)
 }
